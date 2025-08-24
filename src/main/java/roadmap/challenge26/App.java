b07ecd34537fded22c5cf845466e50751519e696
josevrius <josevrius@gmail.com>
2025-08-25 01:54:52 +0200

    update

--- src/main/java/roadmap/challenge26/App.java
+++ src/main/java/roadmap/challenge26/App.java
@@ -1,17 +1,24 @@
 package roadmap.challenge26;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class App {
 
-    private enum Play {
+    private enum Sign {
         PIEDRA, PAPEL, TIJERA;
 
-        private boolean winsTo(Play play) {
-            return (this == PIEDRA && play == TIJERA)
-                    || (this == PAPEL && play == PIEDRA)
-                    || (this == TIJERA && play == PAPEL);
+        private boolean winsTo(Sign sign) {
+            return this == PIEDRA && sign == TIJERA
+                    || this == PAPEL && sign == PIEDRA
+                    || this == TIJERA && sign == PAPEL;
         }
+    }
+
+    private record Round(Sign player1, Sign player2) {
+    }
+
+    private record Scores(int player1, int player2) {
     }
 
     private static final String HEADER = """
@@ -20,42 +27,50 @@ public final class App {
             =====================""";
 
     public static void main(String[] args) {
-        List<Play[]> plays = List.of(
-                new Play[]{Play.PIEDRA, Play.TIJERA},
-                new Play[]{Play.TIJERA, Play.PIEDRA},
-                new Play[]{Play.PAPEL, Play.TIJERA},
-                new Play[]{Play.PAPEL, Play.PAPEL},
-                new Play[]{Play.PAPEL, Play.PIEDRA},
-                new Play[]{Play.TIJERA, Play.PAPEL},
-                new Play[]{Play.PIEDRA, Play.PAPEL},
-                new Play[]{Play.TIJERA, Play.PIEDRA}
-        );
         System.out.println(HEADER);
-        print(plays);
-        System.out.println("Ganador ..: " + checkWinner(plays));
+        List<Round> rounds = setRounds();
+        Scores game = playGame(rounds);
+        showResult(game);
     }
 
-    private static void print(List<Play[]> plays) {
-        for (Play[] play : plays) {
-            System.out.println("Jugador 1 : " +  play[0]);
-            System.out.println("Jugador 2 : " +  play[1]);
-            System.out.println("-----------");
-        }
+    private static List<Round> setRounds() {
+        List<Round> rounds = new ArrayList<>();
+        rounds.add(new Round(Sign.PIEDRA, Sign.PIEDRA));
+        rounds.add(new Round(Sign.TIJERA, Sign.PAPEL));
+        rounds.add(new Round(Sign.PAPEL, Sign.PIEDRA));
+        rounds.add(new Round(Sign.TIJERA, Sign.PIEDRA));
+
+        return rounds;
     }
 
-    private static String checkWinner(List<Play[]> plays) {
-        int p1Count = 0;
-        int p2Count = 0;
+    private static Scores playGame(List<Round> rounds) {
+        int p1Score = 0;
+        int p2Score = 0;
 
-        for (Play[] player : plays) {
-            if (!player[0].equals(player[1])) {
-                if (player[0].winsTo(player[1])) {
-                    p1Count++;
+        for (int i = 0; i < rounds.size(); i++) {
+            Round round = rounds.get(i);
+            System.out.printf("Ronda %d : %s vs %s%n", i + 1, round.player1, round.player2);
+            if (round.player1 != round.player2) {
+                if (round.player1.winsTo(round.player2)) {
+                    p1Score++;
                 } else {
-                    p2Count++;
+                    p2Score++;
                 }
             }
         }
-        return p1Count > p2Count ? "JUGADOR 1" : p1Count < p2Count ? "JUGADOR 2" : "EMPATE";
+        return new Scores(p1Score, p2Score);
+    }
+
+    private static void showResult(Scores score) {
+        String result;
+
+        if (score.player1 > score.player2) {
+            result = "Jugador 1";
+        } else if (score.player1 < score.player2) {
+            result = "Jugador 2";
+        } else {
+            result = "Empate";
+        }
+        System.out.println("Ganador : " + result);
     }
 }
